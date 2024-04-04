e85b083b3a4245fa9d8d5117c58d7c7676ade50d
Zachary Anderson <zachta2020@gamil.com>
2024-04-04 15:11:08 -0500

    fix: add exception handling and robustness to main script

--- main.py
+++ main.py
@@ -20,26 +20,36 @@ def scanPatreonTarget(target):
     print(f"Closing {target}...")
     scanner.driver.close()
 
-if len(sys.argv) < 2:
-    print("Error: Target(s) not specified.")
-    print("Quitting...")
-else:
-    targets = []
-
-    if sys.argv[1] == "-f":
-        inputFile = sys.argv[2]
-
-        with open(inputFile, "r") as f:
-            for line in f:
-                targets.append(line.strip())
-
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Error: Target(s) not specified.\nExitting...")
     else:
-        targets = sys.argv.copy()
-        targets.pop(0)
+        targets = []
 
-    for target in targets:
-        scanPatreonTarget(target)
-        print()
+        if sys.argv[1] == "-f":
+            if len(sys.argv) < 3:
+                print("Error: No file name specified.\nExitting...")
+                exit(0)
 
+            inputFile = sys.argv[2]
 
-    print("Done.")
+            try:
+                with open(inputFile, "r") as f:
+                    for line in f:
+                        targets.append(line.strip())
+            except FileNotFoundError:
+                print("Error: Input file not found.\nExitting...")
+                exit(0)
+
+        else:
+            targets = sys.argv.copy()
+            targets.pop(0)
+
+        for target in targets:
+            if target == "":
+                print("Error: Empty Target! Skipping...")
+            else:
+                scanPatreonTarget(target)
+                print()
+
+        print("Done.")
