aea432fb0dba40a40cf88108ad7fb59df5fd3f1e
Zachary Anderson <zachta2020@gamil.com>
2024-07-22 15:19:51 -0500

    !feat: integrate YouTube Scanning into main script and change format of usage

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from patreon.patreon_scanner import patreonScanner
 from patreon.exceptions import ParseFailedException
+from youtube.youtube_scanner import youtubeScanner
 
-import sys
+from argparse import ArgumentParser, SUPPRESS
 
-def scanPatreonTarget(target):
-    scanner = patreonScanner(target)
+modes = {
+    "patreon" : "Patreon",
+    "youtube" : "YouTube"
+    }
+
+def scanTarget(target, mode):
+    scanner = None
+    if mode == "patreon":
+        scanner = patreonScanner(target)
+    elif mode == "youtube":
+        scanner = youtubeScanner(target)
     try:
         scanner.open()
         print()
@@ -20,7 +30,7 @@ def scanPatreonTarget(target):
     print(f"Closing {target}...")
     scanner.driver.close()
 
-if __name__ == "__main__":
+""" if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Error: Target(s) not specified.\nExitting...")
     else:
@@ -52,4 +62,51 @@ if __name__ == "__main__":
                 scanPatreonTarget(target)
             print()
 
-        print("Done.")
+        print("Done.") """
+
+parser = ArgumentParser(
+    prog="cmdline",
+    description="""
+        Scrapes information of target user(s) from a specified social media or content creator focused website.
+        Avaiable websites: Patreon, Youtube
+    """,
+    argument_default=SUPPRESS
+)
+parser.add_argument("mode", help="sets the target website from the list available")
+
+userTargetGroup = parser.add_mutually_exclusive_group(required=True)
+userTargetGroup.add_argument("-t", "--targets", nargs="+", help="<Required> sets target user(s) to be scraped")
+userTargetGroup.add_argument("-f", "--file", help="<Required> reads target user(s) from specified file")
+
+args = parser.parse_args()
+mode = args.mode.lower()
+
+if mode in modes:
+    currentMode = modes[mode]
+    print(f"Mode: {currentMode} selected.")
+
+    targets = []
+
+    try: #if file was specified
+        file = args.file
+        try:
+            with open(file, "r") as f:
+                for line in f:
+                    targets.append(line.strip())
+        except FileNotFoundError:
+            print("Error: Input file not found.\nExitting...")
+            exit(0)
+    except AttributeError: #if targets were directly inputted on cmdline
+        targets = args.targets
+    
+    for target in targets:
+            if target == "":
+                print("Error: Empty Target! Skipping...")
+            else:
+                scanTarget(target, mode)
+            print()
+
+else:
+    print("ERROR: INVALID MODE SELECTED")
+
+print("Done.")
