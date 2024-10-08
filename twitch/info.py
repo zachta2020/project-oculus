f6f9cf444bcc2f881f1fe592f92cec9597cd82dd
Zachary Anderson <zachta2020@gamil.com>
2024-10-07 20:12:37 -0500

    feat: add ability to detect if target is currently streaming, and if so scan the current stream

--- twitch/info.py
+++ twitch/info.py
@@ -7,6 +7,8 @@ class TwitchInfo(Info):
         self.title: str = None
         self.followerCount: str = None
 
+        self.currentStream: TwitchStream = None
+
     def csv(self) -> str:
         csv: str = ""
 
@@ -21,5 +23,31 @@ class TwitchInfo(Info):
 
         info += f"Channel Title: {self.title}\n"
         info += f"Followers: {self.followerCount}\n"
+        
+        if self.currentStream is not None:
+            info += "\n"
+            info += f"{self.currentStream}\n"
+
+        return info
+
+class TwitchStream:
+    def __init__(self, title:str = None, game:str = None, tags:list[str] = None, viewerCount:int = None, currentRuntime:str = None):
+        self.title: str = title
+        self.game: str = game
+        self.tags: list[str] = tags
+        self.viewerCount: int = viewerCount
+        self.currentRuntime: str = currentRuntime
+
+    def __str__(self) -> str:
+        info: str = ""
+
+        info += f"Current Stream: {self.title}\n"
+        info += f"Game: {self.game}\n"
+
+        tagstring = ", ".join(self.tags)
+        info += f"Tags: {tagstring}\n"
+
+        info += f"Current Viewers: {self.viewerCount}\n"
+        info += f"Runtime: {self.currentRuntime}"
 
         return info
--- twitch/scanner.py
+++ twitch/scanner.py
@@ -7,7 +7,7 @@ from selenium.common.exceptions import (
 from selenium.webdriver.common.by import By
 
 from common.scanner import Scanner
-from twitch.info import TwitchInfo
+from twitch.info import TwitchInfo, TwitchStream
 
 import time
 
@@ -41,21 +41,43 @@ class TwitchScanner(Scanner):
 
             #self.driver.save_screenshot("secret/twitch.png")
 
-            basicInfo = self.driver.find_element(By.ID, "offline-channel-main-content")
+            try: #assuming the streamer is offline
+                basicInfo = self.driver.find_element(By.ID, "offline-channel-main-content")
 
-            title = basicInfo.find_element(By.XPATH, "//h1")
-            followers = basicInfo.find_element(By.XPATH, "div[2]/div[1]/div[1]/div[2]/p")
+                title = basicInfo.find_element(By.XPATH, "//h1")
+                followers = basicInfo.find_element(By.XPATH, "div[2]/div[1]/div[1]/div[2]/p")
 
-            #print(f"{title.text}\n{followers.text}")
+                #print(f"{title.text}\n{followers.text}")
 
-            self.info.title = title.text
-            self.info.followerCount = followers.text.replace(" followers", "")
+                self.info.title = title.text
+                self.info.followerCount = followers.text.replace(" followers", "")
+            except NoSuchElementException: #if the streamer is live
+                basicInfo = self.driver.find_element(By.ID, "live-channel-about-panel")
+                aboutPanel = basicInfo.find_element(By.XPATH, "div[1]/div[2]/div[1]/div[1]")
 
+                title = aboutPanel.find_element(By.XPATH, "div[1]/div[1]/h3")
+                followers = aboutPanel.find_element(By.XPATH, "div[2]/div/div/div/div/div/span/div/div/span")
 
+                #print(followers.text)
 
+                self.info.title = title.text.replace("About ", "")
+                self.info.followerCount = followers.text
 
+                #scan current stream since the streamer is live
+                stream = self.driver.find_element(By.ID, "live-channel-stream-information")
+                streamInfo = stream.find_element(By.XPATH, "div/div/div[2]/div/div[2]/div[2]")
 
+                streamTitle = streamInfo.find_element(By.XPATH, "div[1]/div/div[1]")
+                streamGame = streamInfo.find_element(By.XPATH, "div[1]/div/div[2]/div/div/div[1]")
+                streamTags = streamInfo.find_element(By.XPATH, "div[1]/div/div[2]/div/div/div[2]")
 
+                streamViewers = streamInfo.find_element(By.XPATH, "div[2]/div/div/div[1]/div[1]")
+                streamRuntime = streamInfo.find_element(By.XPATH, "div[2]/div/div/div[1]/div[2]")
+
+                taglist = streamTags.text.split("\n")
+                viewerCount = int(streamViewers.text.replace(",", ""))
+
+                self.info.currentStream = TwitchStream(title=streamTitle.text, game=streamGame.text, tags=taglist, viewerCount=viewerCount, currentRuntime=streamRuntime.text)
 
         except TimeoutException:
             print("ERROR")
