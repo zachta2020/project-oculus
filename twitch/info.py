9d96f946bd45a290ecdfdfded9127fceaed80de5
Zachary Anderson <zachta2020@gamil.com>
2024-10-08 16:08:09 -0500

    feat: add ability to scan information from about page and record to csv

--- twitch/info.py
+++ twitch/info.py
@@ -4,17 +4,37 @@ class TwitchInfo(Info):
     def __init__(self):
         super().__init__()
 
+        #home page
         self.title: str = None
         self.followerCount: str = None
-
         self.currentStream: TwitchStream = None
+
+        #about page
+        self.description: str = None
+        self.team: str = None
+        self.externalLinks: list[tuple[str, str]] = []
+
+    def get_linksString(self) -> str:
+        linkStrings = [f"{link[0]} - {link[1]}" for link in self.externalLinks]
+        return "\n".join(linkStrings)
 
     def csv(self) -> str:
         csv: str = ""
 
         csv += f"Scan Date,{self.get_date()}\n"
         csv += f"Twitch Channel,{self.title}\n"
+        sanitizedDescription = self.description.replace("\"", "\"\"")
+        csv += f"Description,\"{sanitizedDescription}\"\n"
+        csv += f"Team,{self.team}\n"
         csv += f"Followers,{self.followerCount}\n"
+
+        if len(self.externalLinks) > 0:
+            csv += "External,Website,Link\n"
+            for link in self.externalLinks:
+                csv += f",{link[0]},{link[1]}\n"
+
+        if self.currentStream is not None:
+            csv += self.currentStream.csv()
 
         return csv
     
@@ -22,11 +42,18 @@ class TwitchInfo(Info):
         info: str = ""
 
         info += f"Channel Title: {self.title}\n"
-        info += f"Followers: {self.followerCount}\n"
+        info += f"Description: {self.description}\n"
+        info += f"Team: {self.team}\n"
+        info += f"Followers: {self.followerCount}"
+
+        if len(self.externalLinks) > 0:
+            info += "\n\n"
+            info += "External Links:\n"
+            info += self.get_linksString()
         
         if self.currentStream is not None:
-            info += "\n"
-            info += f"{self.currentStream}\n"
+            info += "\n\n"
+            info += f"{self.currentStream}"
 
         return info
 
@@ -38,15 +65,28 @@ class TwitchStream:
         self.viewerCount: int = viewerCount
         self.currentRuntime: str = currentRuntime
 
+    def get_tagstring(self) -> str:
+        return ", ".join(self.tags)
+
+    def csv(self) -> str:
+        csv: str = ""
+
+        csv += "CURRENT LIVESTREAM\n"
+        sanitizedTitle = self.title.replace("\"", "\"\"")
+        csv += f"Title,\"{sanitizedTitle}\"\n"
+        csv += f"Game,{self.game}\n"
+        csv += f"Tags,\"{self.get_tagstring()}\"\n"
+        csv += f"Viewer Count,{self.viewerCount}\n"
+        csv += f"Runtime,{self.currentRuntime}\n"
+
+        return csv
+
     def __str__(self) -> str:
         info: str = ""
 
         info += f"Current Stream: {self.title}\n"
         info += f"Game: {self.game}\n"
-
-        tagstring = ", ".join(self.tags)
-        info += f"Tags: {tagstring}\n"
-
+        info += f"Tags: {self.get_tagstring()}\n"
         info += f"Current Viewers: {self.viewerCount}\n"
         info += f"Runtime: {self.currentRuntime}"
 
--- twitch/scanner.py
+++ twitch/scanner.py
@@ -6,6 +6,8 @@ from selenium.common.exceptions import (
 )
 from selenium.webdriver.common.by import By
 
+from cleantext import clean
+
 from common.scanner import Scanner
 from twitch.info import TwitchInfo, TwitchStream
 
@@ -25,6 +27,7 @@ class TwitchScanner(Scanner):
         
     def run(self):
         self.__scanHome()
+        self.__scanAbout()
 
     def __scanHome(self):
         fullURL = self.getFullURL()
@@ -52,6 +55,8 @@ class TwitchScanner(Scanner):
                 self.info.title = title.text
                 self.info.followerCount = followers.text.replace(" followers", "")
             except NoSuchElementException: #if the streamer is live
+                print("Livestream Detected.")
+
                 basicInfo = self.driver.find_element(By.ID, "live-channel-about-panel")
                 aboutPanel = basicInfo.find_element(By.XPATH, "div[1]/div[2]/div[1]/div[1]")
 
@@ -82,5 +87,53 @@ class TwitchScanner(Scanner):
         except TimeoutException:
             print("ERROR")
 
-    def record(self):
-        print("RECORDING DISABLED.")
+    def __scanAbout(self):
+        print("Scanning About Page...")
+
+        self.driver.get(self.getFullURL() + "/about")
+
+        try:
+            WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.ID, "offline-channel-main-content"))
+            )
+
+            time.sleep(1)
+
+            about = self.driver.find_element(By.ID, "offline-channel-main-content")
+            basicAbout = about.find_element(By.XPATH, "div[3]/div/div/div/div[1]/div[2]/div/div/div[2]/div")
+            panels = about.find_element(By.XPATH, "div[3]/div/div/div/div[2]")
+
+            # since not every streamer has a team, this method is used to detect if there's a team
+            # named in the streamer's about page. This method assumes that the team name always appears
+            # last in the string
+            aboutStatString = basicAbout.find_element(By.XPATH, "div[1]/div")
+            aboutStatList = aboutStatString.text.split("\n")
+            team = None
+            if len(aboutStatList) >= 4:
+                team = aboutStatList[3]
+
+            description = basicAbout.find_element(By.XPATH, "div[1]/p")
+
+            linkList = []
+            try:
+                linkContainer = basicAbout.find_element(By.XPATH, "div[2]/div/div/div")
+                links = linkContainer.find_elements(By.XPATH, "div")
+                
+                for link in links:
+                    anchor = link.find_element(By.XPATH, "div[1]/div/a")
+                    href = anchor.get_attribute("href")
+                    #print(f"link: {anchor.text} - {href}")
+                    cleanLinkName = clean(link.text, no_emoji=True, lower=False)
+                    linkList.append((cleanLinkName, href))
+            except NoSuchElementException:
+                print("No External Links Detected. Proceeding...")
+
+            self.info.team = team
+            self.info.description = description.text
+            self.info.externalLinks = linkList
+
+        except TimeoutException:
+            print("ERROR")
+
+    """ def record(self):
+        print("RECORDING DISABLED.") """
